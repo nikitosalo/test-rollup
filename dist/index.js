50dc6a8aa48b6294aa746708cc00031f196a71c4
Nikitosalo <salikov@ilink.dev>
2023-05-19 18:47:09 +0700

    feat: test rollup

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,1 @@
-export{TestComponent}from"./kit/TestComponent/index.js";import"./index-ea5e2cac.js";
+export{TestComponent}from"./kit/TestComponent/index.js";export{Button}from"./kit/Button/index.js";import"./index-ea5e2cac.js";
